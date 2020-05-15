8825297725622ae4136778568a5711b8c4975bdd
Philippe Timothe <philippetimothe@gmail.com>
2020-05-15 20:02:07 +0000

    Automatically backed up by Learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -20,7 +20,7 @@ class Transfer
       sender.balance = sender.balance - amount
       receiver.balance = receiver.balance + amount
       @status = "complete"
-    elsif !valid? || @status = "complete"
+    elsif !valid? || @status = "complete" || @status = "rejected"
       @satus = "rejected"
       "Transaction rejected. Please check your account balance."
     end
