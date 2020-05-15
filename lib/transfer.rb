6258e0b02da4180bb55fb30690849f9265266d48
Philippe Timothe <philippetimothe@gmail.com>
2020-05-15 19:50:40 +0000

    Automatically backed up by Learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -16,11 +16,10 @@ class Transfer
   end
   
   def execute_transaction
-    if valid? && sender.balance > amount
+    valid? && sender.balance > amount
       sender.balance = sender.balance - amount
       receiver.balance = receiver.balance + amount
       @status = "complete"
-    end
     
    !sender.valid?
       @satus = "rejected"
