b50a0bce582708dd193cd47b4dce104044d86a25
Philippe Timothe <philippetimothe@gmail.com>
2020-05-15 18:50:15 +0000

    Automatically backed up by Learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -20,8 +20,8 @@ class Transfer
     sender.balance = sender.balance - amount
     receiver.balance = receiver.balance + amount
     @status = "complete"
-  end
- #   binding.pry
+  
+    binding.pry
   end
   
  # if !sender.valid? 
