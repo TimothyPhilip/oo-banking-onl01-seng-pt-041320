9721526872413717c178fcea18715fa9e66eec6f
Philippe Timothe <philippetimothe@gmail.com>
2020-05-15 20:05:20 +0000

    Automatically backed up by Learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -29,7 +29,7 @@ class Transfer
   end
   
   def reverse_transfer
-    if execute_transaction == true
+    if execute_transaction == false
     receiver.balance = receiver.balance - amount
     sender.balance = sender.balance + amount
     @status = "reversed"
